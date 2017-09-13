f5f39031f459ec6a596b3b1ce31c2c01a6d41197
Greg Wilson <greg@datacamp.com>
2017-09-13 15:51:06 -0400

    Pushing setup scripts to datasets

--- requirements.sh
+++ requirements.sh
@@ -7,19 +7,18 @@ python3 -c "import sys; print('sys.version:', sys.version); import git; print('g
 # Echo shell commands as they are executed.
 set -x
 
-# Find out where this script is.
-BASEDIR=$(dirname "$0")
-
 # Set up repositories.  These commands are in separate scripts so that
 # we only have to 'sudo' once per script, and so that files are owned
 # by the right user even when created with redirection.
 
+# Download scripts.
+
 # Create repository owned by "repl" user.
-sudo -u repl -i bash ${BASEDIR}/run-as-repl.sh
+sudo -u repl -i bash /home/repl/run-as-repl.sh
 
 # Create the "thunk" user with a home directory and run their script.
 sudo useradd -m thunk
-sudo -u thunk -i bash ${BASEDIR}/run-as-thunk.sh
+sudo -u thunk -i bash /home/repl/run-as-thunk.sh
 
 # Report.
 ls -lR /home
