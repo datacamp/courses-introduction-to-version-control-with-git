7663a3be55485530238508e20ee4c873ed03ed98
Greg Wilson <greg@datacamp.com>
2017-09-13 15:54:03 -0400

    Setting ZIP file path

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Definitions.
+COURSE_ID=course_5355
+ARCHIVE=setup.zip
+
 # Package installation.
 pip3 install gitpython
 python3 -c "import sys; print('sys.version:', sys.version); import git; print('gitpython version:', git.__version__)"
@@ -11,7 +15,11 @@ set -x
 # we only have to 'sudo' once per script, and so that files are owned
 # by the right user even when created with redirection.
 
-# Download scripts.
+# Get the zip file.
+cd /home/repl
+wget https://s3.amazonaws.com/assets.datacamp.com/production/${COURSE_ID}/datasets/${ARCHIVE}
+unzip ${ARCHIVE}
+rm ${ARCHIVE}
 
 # Create repository owned by "repl" user.
 sudo -u repl -i bash /home/repl/run-as-repl.sh
