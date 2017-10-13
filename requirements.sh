977db44712986c376c45d5734fb7a096214844bb
Greg Wilson <greg@datacamp.com>
2017-10-13 06:47:39 -0400

    Extending requirements for disk image

--- requirements.sh
+++ requirements.sh
@@ -2,18 +2,37 @@
 
 # Definitions.
 HOME_DIR=/home/repl
+HOME_COPY=/.course_home
 USER_GROUP=repl:repl
 COURSE_ID=course_5355
 ARCHIVE=setup.zip
+PYTHON=python3
+PIP=pip3
+SHELLWHAT_EXT=git+https://github.com/gvwilson/shellwhat_ext.git
 
-# Package installation.
-pip3 install gitpython
-python3 -c "import sys; print('sys.version:', sys.version); import git; print('gitpython version:', git.__version__)"
+# Report start.
+echo ''
+echo '----------------------------------------'
+echo 'START TIME:' $(date)
+echo 'HOME_DIR: ' ${HOME_DIR}
+echo 'HOME_COPY: ' ${HOME_COPY}
+echo 'USER_GROUP: ' ${USER_GROUP}
+echo 'COURSE_ID: ' ${COURSE_ID}
+echo 'ARCHIVE: ' ${ARCHIVE}
+echo 'This message is to force a rebuild 9.'
+echo
 
 # Make sure we have nano and unzip.
 apt-get update
 apt-get -y install nano
 apt-get -y install unzip
+
+# Python package installation.
+${PIP} install gitpython
+${PIP} install ${SHELLWHAT_EXT}
+${PYTHON} -c "import sys; print('sys.version:', sys.version)"
+${PYTHON} -c "import git; print('gitpython version:', git.__version__)"
+${PYTHON} -c "import shellwhat_ext; print('shellwhat_ext version:', shellwhat_ext.__version__)"
 
 # Echo shell commands as they are executed.
 set -x
@@ -38,8 +57,17 @@ sudo -u thunk -i bash ${HOME_DIR}/run-as-thunk.sh
 # Remove scripts.
 rm -f ${HOME_DIR}/run-as-repl.sh ${HOME_DIR}/run-as-thunk.sh
 
+# Make copy for resetting exercises (done here because chown requires root).
+# Files there will replace /home/repl each exercise.
+# IMPORTANT: Trailing slashes after directory names force rsync to do the right thing.
+rsync -a ${HOME_DIR}/ ${HOME_COPY}/
+chown -R ${USER_GROUP} ${HOME_COPY}
+
 # Report.
 echo "Home directory:"
 ls -lR /home
 echo "Dental repository log:"
 git -C ${HOME_DIR}/dental log | cat
+
+echo 'Home backup directory:'
+ls -R ${HOME_COPY}
